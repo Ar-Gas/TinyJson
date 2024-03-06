7903566b7757959c517bba589bb546a39a2e4ca8
Ar-Gas <2934060590@qq.com>
2024-03-06 22:27:49 +0800

    初始分离

--- main.cpp
+++ main.cpp
@@ -1,221 +1,200 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define INT 0
-#define DOU 1
-#define STR 2
-#define BOOL 3
-#define LIST 4
-#define DICT 5
-class Val {
-public:
-    int INT_VAL;
-    double DOU_VAL;
-    string STR_VAL;
-    bool BOOL_VAL;
-    vector<Val>List;
-    map<Val, Val>dict;
+#include <variant>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <string_view>
+#include <optional>
+#include <regex>
+#include <charconv>
+#include "print.h"
 
-    int type;
+struct JSONObject;
 
-    void del() {
-        if (type == STR)STR_VAL.clear();
-        if (type == LIST)List.clear();
-        if (type == DICT)dict.clear();
-    }
-    Val() {}
-    ~Val() {
-        del();
-    }
-    Val(const int &x) {
-        del();
-        INT_VAL = x;
-        type = INT;
+using JSONDict = std::unordered_map<std::string, JSONObject>;
+using JSONList = std::vector<JSONObject>;
+
+struct JSONObject {
+    std::variant
+            < std::nullptr_t  // null
+                    , bool            // true
+                    , int             // 42
+                    , double          // 3.14
+                    , std::string     // "hello"
+                    , JSONList        // [42, "hello"]
+                    , JSONDict        // {"hello": 985, "world": 211}
+            > inner;
+
+    void do_print() const {
+        printnl(inner);
     }
 
-    Val(const double & x) {
-        del();
-        DOU_VAL = x;
-        type = DOU;
+    template <class T>
+    bool is() const {
+        return std::holds_alternative<T>(inner);
     }
 
-    Val(const string & x) {
-        del();
-        STR_VAL = x;
-        type = STR;
+    template <class T>
+    T const &get() const {
+        return std::get<T>(inner);
     }
 
-    Val(const char *x) {
-        del();
-        type = STR;
-        STR_VAL = string(x);
+    template <class T>
+    T &get() {
+        return std::get<T>(inner);
     }
-
-    Val(const bool & x) {
-        del();
-        BOOL_VAL = x;
-        type = BOOL;
-    }
-
-    Val(initializer_list<Val> l) {
-        del();
-        type = LIST;
-        for (Val x : l)List.push_back(x);
-    }
-
-    void add(Val x) {
-        if (type == LIST)List.push_back(x);
-    }
-
-    void put(Val key, Val val) {
-        if (type == DICT)dict[key] = val;
-    }
-
-    Val& operator [](Val i) {
-        if (type == LIST)return List[i.INT_VAL];
-        else return dict[i];
-    }
-
-    string str();
 };
 
-
-
-ostream& operator << (ostream & out, const Val & v) {
-    if (v.type == INT)out << v.INT_VAL;
-    if (v.type == DOU)out << v.DOU_VAL;
-    if (v.type == STR)out << "\"" << v.STR_VAL << "\"";
-    if (v.type == BOOL) {
-        if (v.BOOL_VAL)out << "true";
-        else out << "false";
+template <class T>
+std::optional<T> try_parse_num(std::string_view str) {
+    T value;
+    auto res = std::from_chars(str.data(), str.data() + str.size(), value);
+    if (res.ec == std::errc() && res.ptr == str.data() + str.size()) {
+        return value;
     }
-    if (v.type == LIST) {
-        out << "[";
-        for (int i = 0; i < v.List.size(); i++) {
-            if (i)out << ",";
-            out << v.List[i];
+    return std::nullopt;
+}
+
+char unescaped_char(char c) {
+    switch (c) {
+        case 'n': return '\n';
+        case 'r': return '\r';
+        case '0': return '\0';
+        case 't': return '\t';
+        case 'v': return '\v';
+        case 'f': return '\f';
+        case 'b': return '\b';
+        case 'a': return '\a';
+        default: return c;
+    }
+}
+
+std::pair<JSONObject, size_t> parse(std::string_view json) {
+    if (json.empty()) {
+        return {JSONObject{std::nullptr_t{}}, 0};
+    } else if (size_t off = json.find_first_not_of(" \n\r\t\v\f\0"); off != 0 && off != json.npos) {
+        auto [obj, eaten] = parse(json.substr(off));
+        return {std::move(obj), eaten + off};
+    } else if ('0' <= json[0] && json[0] <= '9' || json[0] == '+' || json[0] == '-') {
+        std::regex num_re{"[+-]?[0-9]+(\\.[0-9]*)?([eE][+-]?[0-9]+)?"};
+        std::cmatch match;
+        if (std::regex_search(json.data(), json.data() + json.size(), match, num_re)) {
+            std::string str = match.str();
+            if (auto num = try_parse_num<int>(str)) {
+                return {JSONObject{*num}, str.size()};
+            }
+            if (auto num = try_parse_num<double>(str)) {
+                return {JSONObject{*num}, str.size()};
+            }
         }
-        out << "]";
-    }
-    if (v.type == DICT) {
-        out << "{";
-        for (auto it = v.dict.begin(); it != v.dict.end(); it++) {
-            if (it != v.dict.begin())out << ",";
-            out << it->first << ":" << it->second;
+    } else if (json[0] == '"') {
+        std::string str;
+        enum {
+            Raw,
+            Escaped,
+        } phase = Raw;
+        size_t i;
+        for (i = 1; i < json.size(); i++) {
+            char ch = json[i];
+            if (phase == Raw) {
+                if (ch == '\\') {
+                    phase = Escaped;
+                } else if (ch == '"') {
+                    i += 1;
+                    break;
+                } else {
+                    str += ch;
+                }
+            } else if (phase == Escaped) {
+                str += unescaped_char(ch);
+                phase = Raw;
+            }
         }
-        out << "}";
-    }
-    return out;
-}
-
-string Val::str() {
-    stringstream ss; ss << (*this);
-    return ss.str();
-}
-
-bool operator < (const Val &a, const Val &b) {
-    if (a.type != b.type)return a.type < b.type;
-    else {
-        if (a.type == INT)return a.INT_VAL < b.INT_VAL;
-        if (a.type == DOU)return a.DOU_VAL < b.DOU_VAL;
-        if (a.type == STR)return a.STR_VAL < b.STR_VAL;
-        if (a.type == LIST)return a.List < b.List;
-        if (a.type == DICT)return a.dict < b.dict;
-        return true;
-    }
-}
-
-stringstream ss;
-Val parser_val();
-Val parser_num();
-Val parser_str();
-Val parser_bool();
-Val parser_arr();
-Val parser_map();
-
-Val parser_num() {
-    string s;
-    while (isdigit(ss.peek()) || ss.peek() == 'e' || ss.peek() == '-' || ss.peek() == '+')s.push_back(ss.get());
-    if (count(s.begin(), s.end(), '.') || count(s.begin(), s.end(), 'e')) {
-        return stof(s);
-    }
-    else {
-        return stoi(s);
-    }
-}
-
-Val parser_str() {
-    ss.get();
-    string s;
-    while (ss.peek() != '"')s.push_back(ss.get());
-    ss.get();
-    return Val(s);
-}
-
-Val parser_bool() {
-    if (ss.peek() == 'f') {
-        ss.get(); ss.get(); ss.get(); ss.get(); ss.get();
-        return Val(false);
-    }
-    else {
-        ss.get(); ss.get(); ss.get(); ss.get();
-        return Val(true);
-    }
-}
-Val parser_arr() {
-    ss.get();//吃掉  '['
-    Val vec;vec.type = LIST;
-    while (ss.peek() != ']') {
-        vec.add(parser_val());
-        while (ss.peek() != ']' && (ss.peek() == ' ' || ss.peek() == '\t' || ss.peek() == '\n' || ss.peek() == ','))ss.get();
-    }
-    ss.get();
-    return vec;
-}
-
-Val parser_map() {
-    ss.get();
-    Val dict; dict.type = DICT;
-    while (ss.peek() != '}') {
-        Val key = parser_val();
-        while (ss.peek() == ' ' || ss.peek() == ':')ss.get();
-        Val val = parser_val();
-        dict.put(key, val);
-        while (ss.peek() != '}' && (ss.peek() == ' ' || ss.peek() == '\t' || ss.peek() == '\n' || ss.peek() == ','))ss.get();
-    }
-    ss.get();
-    return dict;
-}
-Val parser_val() {
-    while (ss.peek() != -1) {
-        if (ss.peek() == ' ' || ss.peek() == '\n' || ss.peek() == '\t')ss.get();
-        else if (ss.peek() == '"') {
-            return parser_str();
+        return {JSONObject{std::move(str)}, i};
+    } else if (json[0] == '[') {
+        std::vector<JSONObject> res;
+        size_t i;
+        for (i = 1; i < json.size();) {
+            if (json[i] == ']') {
+                i += 1;
+                break;
+            }
+            auto [obj, eaten] = parse(json.substr(i));
+            if (eaten == 0) {
+                i = 0;
+                break;
+            }
+            res.push_back(std::move(obj));
+            i += eaten;
+            if (json[i] == ',') {
+                i += 1;
+            }
         }
-        else if (ss.peek() == 'f' || ss.peek() == 't') {
-            return parser_bool();
+        return {JSONObject{std::move(res)}, i};
+    } else if (json[0] == '{') {
+        std::unordered_map<std::string, JSONObject> res;
+        size_t i;
+        for (i = 1; i < json.size();) {
+            if (json[i] == '}') {
+                i += 1;
+                break;
+            }
+            auto [keyobj, keyeaten] = parse(json.substr(i));
+            if (keyeaten == 0) {
+                i = 0;
+                break;
+            }
+            i += keyeaten;
+            if (!std::holds_alternative<std::string>(keyobj.inner)) {
+                i = 0;
+                break;
+            }
+            if (json[i] == ':') {
+                i += 1;
+            }
+            std::string key = std::move(std::get<std::string>(keyobj.inner));
+            auto [valobj, valeaten] = parse(json.substr(i));
+            if (valeaten == 0) {
+                i = 0;
+                break;
+            }
+            i += valeaten;
+            res.try_emplace(std::move(key), std::move(valobj));
+            if (json[i] == ',') {
+                i += 1;
+            }
         }
-        else if (ss.peek() == '[') {
-            return  parser_arr();
-        }
-        else if (ss.peek() == '{') {
-            return parser_map();
-        }
-        else {
-            return parser_num();
-        }
+        return {JSONObject{std::move(res)}, i};
     }
+    return {JSONObject{std::nullptr_t{}}, 0};
+}
+
+template <class ...Fs>
+struct overloaded : Fs... {
+    using Fs::operator()...;
+};
+
+template <class ...Fs>
+overloaded(Fs...) -> overloaded<Fs...>;
+
+int main() {
+    std::string_view str = R"JSON(985.211)JSON";
+    auto [obj, eaten] = parse(str);
+    print(obj);
+    std::visit(
+            overloaded{
+                    [&] (int val) {
+                        print("int is:", val);
+                    },
+                    [&] (double val) {
+                        print("double is:", val);
+                    },
+                    [&] (std::string val) {
+                        print("string is:", val);
+                    },
+                    [&] (auto val) {
+                        print("unknown object is:", val);
+                    },
+            },
+            obj.inner);
     return 0;
-}
-
-Val parser(string s) {
-    ss.clear();
-    ss << s;
-    return parser_val();
-}
-
-int main()
-{
-    ifstream fin("test.txt");
-    stringstream ss; ss << fin.rdbuf();
-    Val x = parser(ss.str());
 }
